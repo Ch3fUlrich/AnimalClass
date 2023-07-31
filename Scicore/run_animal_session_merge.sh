502b88faabc37ec54e672e56bee6feb6f2ede34f
Sergej Maul <maulser@googlemail.com>
2023-07-31 12:16:57 +0200

    improved command_line, added command_list

--- Scicore/run_animal_session_merge.sh
+++ Scicore/run_animal_session_merge.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-#SBATCH --job-name="suite2p_cat"                    #This is the name of your job
-#SBATCH --cpus-per-task=8                  #This is the number of cores reserved
-#SBATCH --mem-per-cpu=30G              #This is the memory reserved per core.
+#SBATCH --job-name="animal_session"                    #This is the name of your job
+#SBATCH --cpus-per-task=16                  #This is the number of cores reserved
+#SBATCH --mem-per-cpu=15G              #This is the memory reserved per core.
 #SBATCH --tmp=50
 
-#SBATCH --time="06:00:00"        #This is the time that your task will run
-#SBATCH --qos="6hours"           #You will run in this queue
+#SBATCH --time="1-00:00:00"        #This is the time that your task will run
+#SBATCH --qos="1day"           #You will run in this queue
+#SBATCH --array=1-200%20        #This is an array job with 200 tasks 
+  !!!!!!!!!                              #with a maximum simultaneous number of 20 tasks
 
 # Paths to STDOUT or STDERR files should be absolute or relative to current working directory
-#SBATCH --output="myrun.o"%j     #These are the STDOUT and STDERR files
-#SBATCH --error="myrun.e"%j
+#SBATCH --output="outputs/animal_session.o"%j     #These are the STDOUT and STDERR files
+#SBATCH --error="animal_session.e"%j
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
-#SBATCH --mail-user="mitelutco@gmail.com"        #You will be notified via email when your task ends or fails
+#SBATCH --mail-user="sergej.maul@unibas.ch"        #You will be notified via email when your task ends or fails
 
 #Remember:
 #The variable $TMPDIR points to the local hard disks in the computing nodes.
@@ -27,4 +29,9 @@ pip install -r /scicore/home/donafl00/mauser00/code/AnimalClass/requirements.txt
 
 conda env export -n suite2p_cat -f job_$SLURM_JOBID_env.yml --no-builds
 
-python /scicore/projects/donafl00-calcium/Users/Sergej/AnimalClass_command_line.py
+python /scicore/projects/donafl00-calcium/Users/Sergej/create_commands_list.py animal_id session_id
+
+# Run the corresponding commands from the file commands.cmd one by one
+# File should be filled with python /scicore/projects/donafl00-calcium/Users/Sergej/AnimalClass_command_line.py animal_id session_id
+!!!!!!!!!!!!!!!!!!!
+$(head -$SLURM_ARRAY_TASK_ID commands.cmd | tail -1) 
