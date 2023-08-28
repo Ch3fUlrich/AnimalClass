f4f79b076a4369d226b45e7093f38ef09be2258c
Sergej Maul <maulser@googlemail.com>
2023-08-28 13:24:32 +0200

    commands

--- Scicore/commands.cmd
+++ Scicore/commands.cmd
@@ -2,14 +2,14 @@ python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_comm
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-008497 20220211
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-008499 20220205
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009191 20220314
-python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-008499 20220130
-python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-008499 20220201
-python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-010473 20220530
-
+python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-010473 20220510
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009191 20220216
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009192 20220307
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009191 20220316
-
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009192 20220307
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009191 20220316
 python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009191 20220215
+
+new:
+python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009191 20220304
+python /scicore/home/donafl00/mauser00/code/AnimalClass/Scicore/AnimalClass_command_line.py DON-009192 20220226
