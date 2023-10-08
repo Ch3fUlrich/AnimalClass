148ca2540955b48aeed9982e08452de61f5d4f6d
Sergej Maul <maulser@googlemail.com>
2023-10-08 09:05:46 +0200

    improved Scicore code and imports

--- Scicore/create_corr.py
+++ Scicore/create_corr.py
@@ -1,58 +1,12 @@
-# imports
-# Statistics
-import seaborn as sns
-import pandas as pd
-import numpy as np
-
-# Plotting
-import matplotlib as mlp
-import matplotlib.pyplot as plt, mpld3 #plotting and html plots
-plt.style.use('dark_background')
-#plt.style.use('default')
-from matplotlib.lines import Line2D
-from matplotlib.patches import Patch
-
-# Regular Expression searching
-import re
-
-# Suite2p for TIFF file analysis
-import suite2p
-from suite2p.run_s2p import run_s2p, default_ops
-from suite2p.registration import register
-
-
-# Used for Popups
-import tkinter as tk
-
-import nest_asyncio
-
-# for progress bar support
-from tqdm import tqdm
-
 # interact with system
 import os
 import sys
-import copy
-
-
-# statistics
-import scipy
-import math
-
-
-# Mesc file analysis
-import h5py
-from tifffile import tifffile, imread
-import pathlib
-
 
 # add root directory to be able to import packages
 # todo: make all packages installable so they can be called/imported by environment
 module_path = os.path.abspath(os.path.join('../'))
 sys.path.append(module_path)
 
-from manifolds.donlabtools.utils.calcium import calcium
-from manifolds.donlabtools.utils.calcium.calcium import *
 from Classes import *
 from Helper import *
 
